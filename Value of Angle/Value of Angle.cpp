f7b4f2a45d5e77792ef1324810b5b1dce43e1172
Michael Contreras <michaelcontrerasr25@gmail.com>
2016-11-27 23:38:00 +0800

    Value of Angle in Sine, Cosine and Tangent.

---
+++ Value of Angle/Value of Angle.cpp
@@ -0,0 +1,19 @@
+//Input the value of angle and display the value in sine, cosine and tangent
+
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+#define pi 3.14159
+using namespace std;
+
+int main()
+{
+	double angle;
+	cout << "Input value of angle: "; cin >> angle;
+	cout << "Value of " << angle << " in sine is = " << sin(angle*pi / 180) << endl;
+	cout << "Value of " << angle << " in cosine is = " << cos(angle*pi / 180) << endl;
+	cout << "Value of " << angle << "in tangent is = " << tan(angle*pi / 180) << endl;
+
+	_getch();
+	return 0;
+}
