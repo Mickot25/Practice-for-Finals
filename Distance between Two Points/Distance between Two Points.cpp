dec4a5e57771308feb0f2cd97591da95cf19fd3b
Michael Contreras <michaelcontrerasr25@gmail.com>
2016-11-28 01:01:00 +0800

    powers must have decimals.

--- Distance between Two Points/Distance between Two Points.cpp
+++ Distance between Two Points/Distance between Two Points.cpp
@@ -12,7 +12,7 @@ int main()
 	cout << "Input coordinates of the first point in the format (X1, Y1): "; cin >> x1 >> y1;
 	cout << "Input coordinates of the second point in the fomrat (X2, Y2): "; cin >> x2 >> y2;
 	
-	double D = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+	double D = sqrt(pow((x1 - x2), 2.0) + pow((y1 - y2), 2.0));
 
 	cout << "Distance = " << D << endl;
 
