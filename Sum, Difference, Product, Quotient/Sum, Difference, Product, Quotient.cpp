f36de37f51b0020b5406269eb183dd5fc1bc3672
Michael Contreras <michaelcontrerasr25@gmail.com>
2016-11-27 23:51:57 +0800

    2 Integers. Display the Sum, Difference, Product, Quotient and Remainder.

---
+++ Sum, Difference, Product, Quotient/Sum, Difference, Product, Quotient.cpp
@@ -0,0 +1,20 @@
+//Write a program that askes the user to enter 2 integers
+//and determine and display their sum, difference, product, quotient and remainder
+
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int i1, i2;
+	cout << "Enter two integers:"; cin >> i1 >> i2;
+	cout << "The sum of " << i1 << " and " << i2 << " is = " << i1 + i2 << endl;
+	cout << "The difference of " << i1 << " to " << i2 << " is = " << i1 - i2 << endl;
+	cout << "The product of " << i1 << " and " << i2 << " is = " << i1*i2 << endl;
+	cout << "The quotient of " << (i1) << " by " << (i2) << " is = " <<  double (i1) / double (i2) << endl;
+
+	_getch();
+	return 0;
+}
