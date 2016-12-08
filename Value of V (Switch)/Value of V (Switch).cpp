035ef6903a23eff92eb96874cd66fe3b5f396e8a
Michael Contreras <michaelcontrerasr25@gmail.com>
2016-12-08 13:22:57 +0800

    Update Value of V (Switch).cpp
    one block commenting

--- Value of V (Switch)/Value of V (Switch).cpp
+++ Value of V (Switch)/Value of V (Switch).cpp
@@ -1,13 +1,15 @@
-//The value of V depends on the values of x and y as follows:
-//V = xyz when x = 1 and 1<y<5
-//V = x+y/z when x = 1 and y >= 5
-//V = |(xy)/z| when x = 2 and y <= 5
-//V = x + sqrt(y+z) when x = 2 and y > 5
-//V = x+y+z for other values of x and y
-//Write a C++ program that accepts two integers x and y from the kbd.
-//Let z = 2.5
-//Use a constant declaration for z and output V with 10 field width and two decimal places.
-//Use switch statement for selection of x values.
+/*
+The value of V depends on the values of x and y as follows:
+V = xyz when x = 1 and 1<y<5
+V = x+y/z when x = 1 and y >= 5
+V = |(xy)/z| when x = 2 and y <= 5
+V = x + sqrt(y+z) when x = 2 and y > 5
+V = x+y+z for other values of x and y
+Write a C++ program that accepts two integers x and y from the kbd.
+Let z = 2.5
+Use a constant declaration for z and output V with 10 field width and two decimal places.
+Use switch statement for selection of x values.
+*/
 
 #include <iostream>
 #include <conio.h>
