bac0e4df0cf34fe3aa38f6dfbab7f4443ffdf10d
Michael Contreras <michaelcontrerasr25@gmail.com>
2016-12-08 18:14:54 +0800

    Weight of Baggage in Finals 2015

---
+++ Weight of Baggage/Weight of Baggage.cpp
@@ -0,0 +1,33 @@
+/*
+An airline company needs a program to compute baggage charges.
+Write a C++ program that asks the user to enter the weight of a passenger's baggage.
+The program should display the weight of the baggage and the baggage charge, which is calculated as follows:
+If the weight of the baggage is 7 kg or less, display "No charge"
+If the baggage weight is greater than 7 kg but less than 20 kg, the charge is 124.75 + 12.65 for each kg over 7 kg.
+If baggage weight is 20 kg or more, the charge is 235.25 plus 15.85 for each kg over 20 kg.
+*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	double weight;
+	cout << "Input weight of baggage in kg: "; cin >> weight;
+
+	cout << "Weight of your baggage is: " << weight << " kg" << endl;
+
+	if (weight >= 0 && weight <= 7)
+		cout << "Baggage charge: No charge" << endl;
+
+	else if (weight > 7 && weight < 20)
+		cout << "Baggage charge: P" << 124.75 + (weight - 7)*12.65 << endl;
+
+	else
+		cout << "Baggage charge: P" << 235.25 + (weight - 20)*15.85 << endl;
+
+	_getch();
+	return 0;
+
+}
