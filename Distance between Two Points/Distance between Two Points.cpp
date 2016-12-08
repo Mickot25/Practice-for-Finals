decc77b8be985a381c0c7ee2d5c66fb38ab82ed4
Michael Contreras <michaelcontrerasr25@gmail.com>
2016-12-08 16:00:29 +0800

    misspelled word

--- Distance between Two Points/Distance between Two Points.cpp
+++ Distance between Two Points/Distance between Two Points.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	double x1, y1, x2, y2;
 	cout << "Input coordinates of the first point in the format (X1, Y1): "; cin >> x1 >> y1;
-	cout << "Input coordinates of the second point in the fomrat (X2, Y2): "; cin >> x2 >> y2;
+	cout << "Input coordinates of the second point in the format (X2, Y2): "; cin >> x2 >> y2;
 	
 	double D = sqrt(pow((x1 - x2), 2.0) + pow((y1 - y2), 2.0));
 
